0f8779ec4c430917698ea7d6afda6860d6be4f72
Nanthavikraman <nanthavikramanms@gmail.com>
2025-02-10 23:19:59 +0530

    Initial commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/nandy/Documents/Simulator/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/nandy/Documents/Simulator/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/nandy/Documents/Simulator/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/nandy/Documents/Simulator/src:$ROS_PACKAGE_PATH"
